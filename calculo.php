805a4129243ee9ef7361e7a0298899103a6421aa
urielrc18 <85496327+urielrc18@users.noreply.github.com>
2023-06-24 18:29:12 -0500

    Add files via upload

---
+++ calculo.php
@@ -0,0 +1,109 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = '';
+$dbname = "bd_tfinal";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $prestamo = $_POST["prestamo"];
+    $cuota_inicial = $_POST["cuota_inicial"];
+    $tea = $_POST["tea"];
+    $plazo = $_POST["plazo"];
+
+    $cuota_inicial = $prestamo * ($cuota_inicial / 100);
+    $saldo_inicial = $prestamo - $cuota_inicial;
+    $tes = pow(1 + ($tea / 100), 0.5) - 1;
+    $total_cuotas = $plazo * 2;
+    $cuota = ($saldo_inicial * $tes) / (1 - pow(1 + $tes, -$total_cuotas));
+
+    $sql = "INSERT INTO prestamos (prestamo, cuota_inicial, tea, plazo, tes, total_cuotas, cuota)
+    VALUES ($prestamo, $cuota_inicial, $tea, $plazo, $tes, $total_cuotas, $cuota)";
+
+    if ($conn->query($sql) === TRUE) {
+        $prestamo_id = $conn->insert_id;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $tabla = '';
+    $saldo_actual = $saldo_inicial;
+    for ($i = 1; $i <= $total_cuotas; $i++) {
+        $interes = $saldo_actual * $tes;
+        $amortizacion = $cuota - $interes;
+        $saldo_final = $saldo_actual - $amortizacion;
+
+        $sql = "INSERT INTO cuotas (prestamo_id, numero_de_cuota, saldo_inicial, interes, cuota, amortizacion, saldo_final)
+        VALUES ($prestamo_id, $i, $saldo_actual, $interes, $cuota, $amortizacion, $saldo_final)";
+
+        if ($conn->query($sql) === FALSE) {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $tabla .= "<tr>
+                        <td>$i</td>
+                        <td>$tea%</td>
+                        <td>$tes%</td>
+                        <td>S</td>
+                        <td>$saldo_actual</td>
+                        <td>$interes</td>
+                        <td>$cuota</td>
+                        <td>$amortizacion</td>
+                        <td>$saldo_final</td>
+                   </tr>";
+
+        $saldo_actual = $saldo_final;
+
+        if ($i == $total_cuotas) {
+            $saldo_final = 0;
+            break;
+        }
+    }
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora de Préstamo - Resultado</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<a class="btn btn-primary" href="index.html">Regresar a Inicio</a>
+    <div class="container">
+        <h1 class="mt-4 mb-3">Calculadora de Préstamo - Resultado</h1>
+
+        <?php if ($_SERVER["REQUEST_METHOD"] == "POST") : ?>
+            <h2>Plan de Pagos</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Número de Cuota</th>
+                        <th>TEA</th>
+                        <th>TES</th>
+                        <th>Plazo Gracia</th>
+                        <th>Saldo Inicial</th>
+                        <th>Interés</th>
+                        <th>Cuota</th>
+                        <th>Amortización</th>
+                        <th>Saldo Final</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php echo $tabla; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
+        <a href="registros.php" class="btn btn-primary mt-3">Ver todos los préstamos</a>
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</body>
+</html>
---
+++ detalles.php
@@ -0,0 +1,45 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = '';
+$dbname = "bd_tfinal";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+$prestamo_id = $_GET['id'];
+$sql = "SELECT * FROM cuotas WHERE prestamo_id = $prestamo_id";
+$result = $conn->query($sql);
+
+$conn->close();
+?>
+
+<h2>Detalles del Préstamo <?php echo $prestamo_id; ?></h2>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Número de Cuota</th>
+            <th>Saldo Inicial</th>
+            <th>Interés</th>
+            <th>Cuota</th>
+            <th>Amortización</th>
+            <th>Saldo Final</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr><td>" . $row["numero_de_cuota"]. "</td><td>" . $row["saldo_inicial"]. "</td><td>" 
+                . $row["interes"]. "</td><td>" . $row["cuota"]. "</td><td>" . $row["amortizacion"]. "</td><td>" 
+                . $row["saldo_final"]. "</td></tr>";
+            }
+        } else {
+            echo "<tr><td colspan='6'>No hay detalles para este préstamo</td></tr>";
+        }
+        ?>
+    </tbody>
+</table>
