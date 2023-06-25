4808d08d56929076734fb6954bdfac0b82d2fe8f
urielrc18 <85496327+urielrc18@users.noreply.github.com>
2023-06-24 22:26:04 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -11,9 +11,12 @@
 <body>
 
 <!-- Aquí comienza tu navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Bonomax</a>
+    <a class="navbar-brand" href="#">
+        Bonomax |
+        <img src="upc-logo-blanco-logo-upc-blanco-1629401.png" alt="Logo" width="30" height="30">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
